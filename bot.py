7c712b8581a01551b459d42130334c3ce05fa766
mushfiqbd <111460070+mushfiqbd@users.noreply.github.com>
2025-03-23 04:18:17 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def save_subscriptions(subscriptions):
 subscriptions = load_subscriptions()
 
 # Admin chat ID (replace with actual admin Telegram chat ID)
-ADMIN_CHAT_ID = "6251161332"
+ADMIN_CHAT_ID = "7809844153"
 
 # Pagination settings
 PAGE_SIZE = 5  # Number of subscriptions per page
